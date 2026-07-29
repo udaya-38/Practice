d5416a7668760b8e7f999d0e0d8dd33c4d69c50a
udaya-38 <udayagiri7734@gmail.com>
2026-07-29 15:37:23 +0530

    Add PrimeNumber

--- src/Java_Practice/PrimeNoRange.java
+++ src/Java_Practice/PrimeNoRange.java
@@ -2,16 +2,32 @@ package Java_Practice;
 
 import java.util.Scanner;
 
-public class PrimeNumber {
+public class PrimeNoRange {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the Number: ");
-        int num = scanner.nextInt();
+//        System.out.print("Enter the Number: ");
+//        int num = scanner.nextInt();
 
-        System.out.println(isPrime(num));
+        System.out.print("Enter the Start: ");
+        int start = scanner.nextInt();
+
+        System.out.print("Enter the End: ");
+        int end = scanner.nextInt();
+
+        isPrimeRange(start,end);
 
         scanner.close();
+    }
+
+    public static void isPrimeRange(int start , int end) {
+
+        for(int i = start ; i <= end ; i++) {
+
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
+        }
     }
 
     public static boolean isPrime(int a) {
@@ -35,4 +51,6 @@ public class PrimeNumber {
 
         return true; // Factor Not found so it is indeed a Prime Number
     }
+
+
 }

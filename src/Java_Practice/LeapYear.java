0ed4499d4adc796a7658bdd384a23a2cdbf1ba47
udaya-38 <udayagiri7734@gmail.com>
2026-07-29 13:30:13 +0530

    javac

---
+++ src/Java_Practice/LeapYear.java
@@ -0,0 +1,17 @@
+package Java_Practice;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the Year: ");
+        int year = scanner.nextInt();
+
+        System.out.println(isLeap(year));
+    }
+
+    public static boolean isLeap(int a) {
+        return (a % 4 == 0 && a % 100 != 0) || (a % 400 == 0);
+    }
+}

c8d3f306e47197403acb4375ddf570a092d2be4f
udaya-38 <udayagiri7734@gmail.com>
2026-07-29 16:08:44 +0530

    java

---
+++ src/Java_Practice/SumDigits.java
@@ -0,0 +1,30 @@
+package Java_Practice;
+
+import java.util.Scanner;
+
+public class SumDigits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the Number: ");
+        int num = scanner.nextInt();
+
+        System.out.println(SumOfDigits(num));
+
+        scanner.close();
+    }
+
+    public static int SumOfDigits(int a) {
+
+        int sum = 0;
+        int rem = 0;
+
+        while (a > 0) {
+
+            rem = a % 10;  // TAKES THE LAST NUMBER EX: 1234 -> 4
+            sum = sum + rem;
+            a = a / 10; // REMOVES THE LAST NUMBER EX: 1234 -> 123
+        }
+
+        return sum;
+    }
+}

22edd5a0f536237033652a3c0a420aac7c438132
udaya-38 <udayagiri7734@gmail.com>
2026-07-29 16:33:38 +0530

    java

---
+++ src/Java_Practice/Palindrome.java
@@ -0,0 +1,32 @@
+package Java_Practice;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the Number: ");
+        int num = scanner.nextInt();
+
+        System.out.println(isPalindrome(num));
+
+        scanner.close();
+    }
+
+    public static boolean isPalindrome(int a) {
+        int rev = 0;
+        int temp = a;
+
+        while (a > 0) {
+            int rem = a % 10;
+            rev = (rev * 10) + rem;
+            a = a / 10;
+        }
+
+        if (temp == rev) {
+            return true;
+        }
+
+        return false;
+    }
+}
